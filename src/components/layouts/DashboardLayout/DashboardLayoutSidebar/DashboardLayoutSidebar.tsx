efb2f77f088d294c9c8f636f9999df0f030e5458
Lazydavidd <dafaraihan062@gmail.com>
2025-07-01 01:54:51 +0700

    feat: DataTable UI

--- src/components/layouts/DashboardLayout/DashboardLayoutSidebar/DashboardLayoutSidebar.tsx
+++ src/components/layouts/DashboardLayout/DashboardLayoutSidebar/DashboardLayoutSidebar.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { JSX } from "react";
 import { cn } from "@/utils/cn";
 import { Button, Listbox, ListboxItem } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { CiLogout } from "react-icons/ci";
+import Link from "next/link";
+
 
 interface SidebarItem {
   key: string;
   label: string;
   href: string;
-  icon: React.ReactNode;
+  icon: JSX.Element;
 }
 
 interface PropTypes {
@@ -18,48 +20,53 @@ interface PropTypes {
   isOpen: boolean;
 }
 
-const DashboardLayoutSidebar = ({ sidebarItems, isOpen }: PropTypes) => {
+const DashboardLayoutSidebar = (props: PropTypes) => {
+  const { sidebarItems, isOpen } = props;
   const router = useRouter();
-
   return (
     <div
       className={cn(
-        "fixed lg:relative z-50 flex h-screen w-full max-w-[300px] -translate-x-full flex-col justify-between border-r-1 border-default-200 bg-white px-4 py-6 transition-all lg:translate-x-0",
-        {
-          "translate-x-0": isOpen,
-        }
+        "fixed z-50 flex h-screen w-full max-w-[300px] -translate-x-full flex-col justify-between border-r-1 border-default-200 bg-white px-4 py-6 transition-all lg:relative lg:translate-x-0",
+        { "translate-x-0": isOpen },
       )}
     >
       <div>
-        <div className="flex justify-center w-full">
+        <div className="flex justify-center">
           <Image
             src="/images/general/logo.svg"
             alt="logo"
             width={180}
             height={60}
-            className="mb-6 cursor-pointer"
+            className="mb-6 w-32"
             onClick={() => router.push("/")}
           />
         </div>
-
-        <Listbox items={sidebarItems} variant="solid" aria-label="Dashboard Menu">
+        <Listbox
+          items={sidebarItems}
+          variant="solid"
+          aria-label="Dashboard Menu"
+        >
           {(item) => (
             <ListboxItem
               key={item.key}
               className={cn("my-1 h-12 text-2xl", {
-                "bg-danger-500 text-white": router.pathname.startsWith(item.href),
+                "bg-danger-500 text-white": router.pathname.startsWith(
+                  item.href,
+                ),
               })}
               startContent={item.icon}
               textValue={item.label}
-              onPress={() => router.push(item.href)}
+              aria-labelledby={item.label}
+              aria-describedby={item.label}
+              as={Link}
+              href={item.href}
             >
               <p className="text-small">{item.label}</p>
             </ListboxItem>
           )}
         </Listbox>
       </div>
-
-      <div className="mt-auto flex items-center p-1">
+      <div className="flex items-center p-1">
         <Button
           color="danger"
           fullWidth
---
+++ src/components/ui/DataTable/DataTable.tsx
@@ -0,0 +1,175 @@
+import {
+  Button,
+  Input,
+  Pagination,
+  Select,
+  SelectItem,
+  Spinner,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import { ChangeEvent, Key, ReactNode, useMemo } from "react";
+import { CiSearch } from "react-icons/ci";
+import { cn } from "@/utils/cn";
+
+const LIMIT_LISTS = [
+  { value: "10", label: "10" },
+  { value: "20", label: "20" },
+  { value: "50", label: "50" },
+];
+
+interface PropTypes {
+  buttonTopContentLabel?: string;
+  columns: Record<string, unknown>[];
+  data: Record<string, unknown>[];
+  emptyContent: string;
+  isLoading?: boolean;
+  onClickButtonTopContent?: () => void;
+  renderCell: (item: Record<string, unknown>, columnKey: Key) => ReactNode;
+  showLimit?: boolean;
+  showSearch?: boolean;
+  totalPages: number;
+  currentPage: number;
+  currentLimit: number;
+  onChangePage: (page: number) => void;
+  onChangeLimit: (e: ChangeEvent<HTMLSelectElement>) => void;
+  onSearch: (e: ChangeEvent<HTMLInputElement>) => void;
+  onClearSearch: () => void;
+}
+
+const DataTable = (props: PropTypes) => {
+  const {
+    buttonTopContentLabel,
+    columns,
+    data,
+    emptyContent,
+    isLoading,
+    onClickButtonTopContent,
+    renderCell,
+    showLimit = true,
+    showSearch = true,
+    totalPages,
+    currentPage,
+    currentLimit,
+    onChangePage,
+    onChangeLimit,
+    onSearch,
+    onClearSearch,
+  } = props;
+
+  const TopContent = useMemo(() => {
+    return (
+      <div className="flex flex-col-reverse items-start justify-between gap-y-4 lg:flex-row lg:items-center">
+        {showSearch && (
+          <Input
+            isClearable
+            className="w-full sm:max-w-[24%]"
+            placeholder="Search by name"
+            startContent={<CiSearch />}
+            onClear={onClearSearch}
+            onChange={onSearch}
+          />
+        )}
+        {buttonTopContentLabel && (
+          <Button color="danger" onPress={onClickButtonTopContent}>
+            {buttonTopContentLabel}
+          </Button>
+        )}
+      </div>
+    );
+  }, [
+    buttonTopContentLabel,
+    onSearch,
+    onClearSearch,
+    onClickButtonTopContent,
+    showSearch,
+  ]);
+
+  const BottomContent = useMemo(() => {
+    return (
+      <div className="flex items-center justify-center lg:justify-between">
+        {showLimit && (
+          <Select
+            className="hidden max-w-36 lg:block"
+            size="md"
+            selectedKeys={[`${currentLimit}`]}
+            selectionMode="single"
+            onChange={onChangeLimit}
+            startContent={<p className="text-small">Show:</p>}
+            disallowEmptySelection
+          >
+            {LIMIT_LISTS.map((item) => (
+              <SelectItem key={item.value} value={item.value}>
+                {item.label}
+              </SelectItem>
+            ))}
+          </Select>
+        )}
+        {totalPages > 1 && (
+          <Pagination
+            isCompact
+            showControls
+            color="danger"
+            page={Number(currentPage)}
+            total={totalPages}
+            onChange={onChangePage}
+            loop
+          />
+        )}
+      </div>
+    );
+  }, [
+    showLimit,
+    currentLimit,
+    currentPage,
+    totalPages,
+    onChangeLimit,
+    onChangePage,
+  ]);
+
+  return (
+    <Table
+      bottomContent={BottomContent}
+      bottomContentPlacement="outside"
+      classNames={{
+        base: "max-w-full",
+        wrapper: cn({ "overflow-x-hidden": isLoading }),
+      }}
+      topContent={TopContent}
+      topContentPlacement="outside"
+    >
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn key={column.uid as Key}>
+            {column.name as string}
+          </TableColumn>
+        )}
+      </TableHeader>
+
+      <TableBody
+        emptyContent={emptyContent}
+        isLoading={isLoading}
+        items={data}
+        loadingContent={
+          <div className="flex h-full w-full items-center justify-center bg-foreground-700/30 backdrop-blur-sm">
+            <Spinner color="danger" />
+          </div>
+        }
+      >
+        {(item: Record<string, unknown>) => (
+          <TableRow key={item._id as Key}>
+            {(columnKey) => (
+              <TableCell>{renderCell(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default DataTable;
