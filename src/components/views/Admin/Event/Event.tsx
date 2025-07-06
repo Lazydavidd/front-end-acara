ec89e804b8b927a0bd9470d0a8aac0081710c8c5
Lazydavidd <dafaraihan062@gmail.com>
2025-07-06 19:54:23 +0700

    feat: Admin Detail Event

---
+++ src/components/views/Admin/Event/Event.tsx
@@ -0,0 +1,114 @@
+import DataTable from "@/components/ui/DataTable";
+import { Chip, useDisclosure } from "@nextui-org/react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Key, ReactNode, useCallback, useEffect, useState } from "react";
+import { COLUMN_LISTS_EVENT } from "./Event.constants";
+import useChangeUrl from "@/hooks/useChangeUrl";
+import useEvent from "./useEvent";
+import DropdownAction from "@/components/commons/DropdownAction";
+import AddEventModal from "./AddEventModal";
+import DeleteEventModal from "./DeleteEventModal";
+
+const Event = () => {
+  const { push, isReady, query } = useRouter();
+  const {
+    dataEvents,
+    isLoadingEvents,
+    isRefetchingEvents,
+    refetchEvents,
+
+    selectedId,
+    setSelectedId,
+  } = useEvent();
+
+  const { setUrl } = useChangeUrl();
+
+  const addEventModal = useDisclosure();
+  const deleteEventModal = useDisclosure();
+
+  useEffect(() => {
+    if (isReady) {
+      setUrl();
+    }
+  }, [isReady]);
+
+  const renderCell = useCallback(
+    (event: Record<string, unknown>, columnKey: Key) => {
+      const cellValue = event[columnKey as keyof typeof event];
+
+      switch (columnKey) {
+        case "banner":
+          return (
+            <Image
+              className="aspect-video w-36 rounded-lg object-cover"
+              src={`${cellValue}`}
+              alt="icon"
+              width={200}
+              height={100}
+            />
+          );
+        case "isPublish":
+          return (
+            <Chip
+              color={cellValue ? "success" : "warning"}
+              size="sm"
+              variant="flat"
+            >
+              {cellValue === true ? "Published" : "Not Published"}
+            </Chip>
+          );
+        case "actions":
+          return (
+            <DropdownAction
+              onPressButtonDetail={() => push(`/admin/event/${event._id}`)}
+              onPressButtonDelete={() => {
+                setSelectedId(`${event._id}`);
+                deleteEventModal.onOpen();
+              }}
+            />
+          );
+        default:
+          return cellValue as ReactNode;
+      }
+    },
+    [push],
+  );
+
+      const [currentPage, setCurrentPage] = useState(1);
+    const [currentLimit, setCurrentLimit] = useState(10);
+    const [search, setSearch] = useState("");
+  return (
+    <section>
+      {Object.keys(query).length > 0 && (
+        <DataTable
+          buttonTopContentLabel="Create Event"
+          columns={COLUMN_LISTS_EVENT}
+          data={dataEvents?.data || []}
+          emptyContent="Event is empty"
+          isLoading={isLoadingEvents || isRefetchingEvents}
+          onClickButtonTopContent={addEventModal.onOpen}
+          renderCell={renderCell}
+          totalPages={dataEvents?.pagination.totalPages}
+
+           currentPage={currentPage}
+          currentLimit={currentLimit}
+          onChangePage={setCurrentPage}
+          onChangeLimit={(e) => setCurrentLimit(Number(e.target.value))}
+          onChangeSearch={(e) => setSearch(e.target.value)}
+          onClearSearch={() => setSearch("")}
+          
+        />
+      )}
+      <AddEventModal {...addEventModal} refetchEvents={refetchEvents} />
+      <DeleteEventModal
+        {...deleteEventModal}
+        selectedId={selectedId}
+        setSelectedId={setSelectedId}
+        refetchEvents={refetchEvents}
+      />
+    </section>
+  );
+};
+
+export default Event;
