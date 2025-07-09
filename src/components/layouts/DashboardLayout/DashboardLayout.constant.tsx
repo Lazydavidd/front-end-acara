3fe9c4a781ac08e1b24fe3ebbe1ceedab78a382d
Lazydavidd <dafaraihan062@gmail.com>
2025-07-09 17:49:55 +0700

    feat: list transaction member

--- src/components/layouts/DashboardLayout/DashboardLayout.constant.tsx
+++ src/components/layouts/DashboardLayout/DashboardLayout.constant.tsx
@@ -1,65 +1,51 @@
-import { CiBookmark, CiGrid41, CiSettings, CiShoppingTag, CiViewList, CiWallet } from "react-icons/ci";
+import {
+  CiBookmark,
+  CiShoppingTag,
+  CiUser,
+  CiViewList,
+  CiWallet,
+} from "react-icons/ci";
 
 const SIDEBAR_ADMIN = [
-    {
-        key: "dashboard",
-        label: "Dashboard",
-        href: "/admin/dashboard",
-        icon: <CiGrid41 />
-    },
-    {
-        key: "event",
-        label: "Event",
-        href: "/admin/event",
-        icon: <CiViewList />
-    },
-     {
-        key: "category",
-        label: "Category",
-        href: "/admin/category",
-        icon: <CiShoppingTag />
-    },
-     {
-        key: "banner",
-        label: "Banner",
-        href: "/admin/banner",
-        icon: <CiBookmark />
-    },
-     {
-        key: "transaction",
-        label: "Transaction",
-        href: "/admin/transaction",
-        icon: <CiWallet />
-    },
-    {
-        key: "setting",
-        label: "Setting",
-        href: "/admin/setting",
-        icon: <CiSettings />
-    },
+  {
+    key: "event",
+    label: "Event",
+    href: "/admin/event",
+    icon: <CiViewList />,
+  },
+  {
+    key: "category",
+    label: "Category",
+    href: "/admin/category",
+    icon: <CiShoppingTag />,
+  },
+  {
+    key: "banner",
+    label: "Banner",
+    href: "/admin/banner",
+    icon: <CiBookmark />,
+  },
+  {
+    key: "transaction",
+    label: "Transaction",
+    href: "/admin/transaction",
+    icon: <CiWallet />,
+  },
 ];
-
 
 const SIDEBAR_MEMBER = [
-    {
-        key: "dashboard",
-        label: "Dashboard",
-        href: "/member",
-        icon: <CiGrid41 />
-    },
-        {
-        key: "transaction",
-        label: "Transaction",
-        href: "/member/transaction",
-        icon: <CiWallet />
-    },
-     {
-        key: "setting",
-        label: "Setting",
-        href: "/member/setting",
-        icon: <CiSettings />
-    },
- 
+  {
+    key: "transaction",
+    label: "Transaction",
+    href: "/member/transaction",
+    icon: <CiWallet />,
+  },
+  {
+    key: "profile",
+    label: "Profile",
+    href: "/member/profile",
+    icon: <CiUser />,
+  },
 ];
 
-export {SIDEBAR_ADMIN, SIDEBAR_MEMBER};
+export { SIDEBAR_ADMIN, SIDEBAR_MEMBER };
